f231da9ab4d8695b2ae4285d2ab324903ac580ad
De3trvctions <keldenong@gmail.com>
2024-11-28 13:12:47 +0800

    feat(New RPC): Login new RPC

--- controllers/system/login.go
+++ controllers/system/login.go
@@ -4,28 +4,23 @@ import (
 	"api-login-proto/login"
 	"fmt"
 	"standard-library/consts"
-	"standard-library/jwt"
 	"standard-library/models/dto"
-	"standard-library/nacos"
 	"standard-library/redis"
 	"standard-library/validation"
-	"strconv"
-	"time"
 
 	"github.com/beego/beego/v2/core/logs"
-	"github.com/beego/beego/v2/server/web"
 )
 
 type LoginController struct {
 	PermissionController
-	GRPCClient login.ActivityChangeLogServiceClient
+	GRPCClient login.UserLoginServiceClient
 }
 
 func (ctl *LoginController) Prepare() {
 	ctl.PermissionController.Prepare()
 	//初始化连接
 	ctl.ConnGRpc("service-login")
-	ctl.GRPCClient = login.NewActivityChangeLogServiceClient(ctl.GrpcConn.Conn())
+	ctl.GRPCClient = login.NewUserLoginServiceClient(ctl.GrpcConn.Conn())
 	if ctl.GRPCClient == nil {
 		ctl.Error(consts.SERVER_ERROR)
 	}
@@ -56,63 +51,63 @@ func (ctl *LoginController) Login() {
 	ctl.CommonJSONRequest(req, ctl.GRPCClient.Login)
 }
 
-func (ctl *LoginController) getRedisLoginStatus(username string) (ableLogin bool, remaindingTime int) {
-	ableLogin = true
-	ex1, _ := redis.Exists(fmt.Sprintf(consts.FailLoginAccountLock, username))
+// func (ctl *LoginController) getRedisLoginStatus(username string) (ableLogin bool, remaindingTime int) {
+// 	ableLogin = true
+// 	ex1, _ := redis.Exists(fmt.Sprintf(consts.FailLoginAccountLock, username))
 
-	if ex1 {
-		ableLogin = false
-		timeCache, _ := redis.Get(fmt.Sprintf(consts.FailLoginAccountLockTime, username))
-		redisTime, _ := strconv.ParseInt(timeCache, 10, 64)
-		timeLeft := time.Unix(redisTime, 0)
-		remaindingTime = int(time.Until(timeLeft).Seconds())
-	}
-	return
-}
+// 	if ex1 {
+// 		ableLogin = false
+// 		timeCache, _ := redis.Get(fmt.Sprintf(consts.FailLoginAccountLockTime, username))
+// 		redisTime, _ := strconv.ParseInt(timeCache, 10, 64)
+// 		timeLeft := time.Unix(redisTime, 0)
+// 		remaindingTime = int(time.Until(timeLeft).Seconds())
+// 	}
+// 	return
+// }
 
-func (ctl *LoginController) setRedisLoginFail(username string) (err error) {
-	failCount := 0
-	ex, _ := redis.Exists(fmt.Sprintf(consts.FailLoginCount, username))
-	if ex {
-		count, _ := redis.Get(fmt.Sprintf(consts.FailLoginCount, username))
-		failCount, _ = strconv.Atoi(count)
-		_, _ = redis.Del(fmt.Sprintf(consts.FailLoginCount, username))
-	}
+// func (ctl *LoginController) setRedisLoginFail(username string) (err error) {
+// 	failCount := 0
+// 	ex, _ := redis.Exists(fmt.Sprintf(consts.FailLoginCount, username))
+// 	if ex {
+// 		count, _ := redis.Get(fmt.Sprintf(consts.FailLoginCount, username))
+// 		failCount, _ = strconv.Atoi(count)
+// 		_, _ = redis.Del(fmt.Sprintf(consts.FailLoginCount, username))
+// 	}
 
-	if failCount >= 5 {
-		_ = redis.Set(fmt.Sprintf(consts.FailLoginCount, username), failCount, time.Duration(15)*time.Minute)
-		_ = redis.Set(fmt.Sprintf(consts.FailLoginAccountLock, username), 1, time.Duration(15)*time.Minute)
-		_ = redis.Set(fmt.Sprintf(consts.FailLoginAccountLockTime, username), time.Now().Add(time.Duration(15)*time.Minute).Unix(), time.Duration(15)*time.Minute)
-	} else {
-		_ = redis.Set(fmt.Sprintf(consts.FailLoginCount, username), failCount)
-	}
-	return
-}
+// 	if failCount >= 5 {
+// 		_ = redis.Set(fmt.Sprintf(consts.FailLoginCount, username), failCount, time.Duration(15)*time.Minute)
+// 		_ = redis.Set(fmt.Sprintf(consts.FailLoginAccountLock, username), 1, time.Duration(15)*time.Minute)
+// 		_ = redis.Set(fmt.Sprintf(consts.FailLoginAccountLockTime, username), time.Now().Add(time.Duration(15)*time.Minute).Unix(), time.Duration(15)*time.Minute)
+// 	} else {
+// 		_ = redis.Set(fmt.Sprintf(consts.FailLoginCount, username), failCount)
+// 	}
+// 	return
+// }
 
-func (ctl *LoginController) delRedisLoginFail(username string) {
-	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginCount, username))
-	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginAccountLock, username))
-	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginAccountLockTime, username))
-}
+// func (ctl *LoginController) delRedisLoginFail(username string) {
+// 	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginCount, username))
+// 	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginAccountLock, username))
+// 	_, _ = redis.Del(fmt.Sprintf(consts.FailLoginAccountLockTime, username))
+// }
 
-func getToken(req dto.ReqLogin, id int64) (token string) {
-	// tokenSalt, _ := web.AppConfig.String("TokenSalt")
-	// tokenExpMinute, _ := web.AppConfig.Int("TokenExpMinute")
-	delToken(req.Username)
-	token = jwt.Gen(web.M{
-		"Username":  req.Username,
-		"AccountId": id,
-		"Ip":        req.IP,
-	}, nacos.TokenSalt, time.Duration(nacos.TokenExpMinute)*time.Minute)
-	setToken(token, req.Username)
-	return
-}
+// func getToken(req dto.ReqLogin, id int64) (token string) {
+// 	// tokenSalt, _ := web.AppConfig.String("TokenSalt")
+// 	// tokenExpMinute, _ := web.AppConfig.Int("TokenExpMinute")
+// 	delToken(req.Username)
+// 	token = jwt.Gen(web.M{
+// 		"Username":  req.Username,
+// 		"AccountId": id,
+// 		"Ip":        req.IP,
+// 	}, nacos.TokenSalt, time.Duration(nacos.TokenExpMinute)*time.Minute)
+// 	setToken(token, req.Username)
+// 	return
+// }
 
-func setToken(token, username string) {
-	// Set token expired as 1 days
-	_ = redis.Set(fmt.Sprintf(consts.AccountLoginByToken, token), username, time.Duration(nacos.TokenExpMinute)*time.Minute)
-	_ = redis.Set(fmt.Sprintf(consts.AccountLoginByUsername, username), token, time.Duration(nacos.TokenExpMinute)*time.Minute)
-}
+// func setToken(token, username string) {
+// 	// Set token expired as 1 days
+// 	_ = redis.Set(fmt.Sprintf(consts.AccountLoginByToken, token), username, time.Duration(nacos.TokenExpMinute)*time.Minute)
+// 	_ = redis.Set(fmt.Sprintf(consts.AccountLoginByUsername, username), token, time.Duration(nacos.TokenExpMinute)*time.Minute)
+// }
 
 func delToken(username string) {
 	ex, _ := redis.Exists(fmt.Sprintf(consts.AccountLoginByUsername, username))
@@ -129,6 +124,12 @@ func delToken(username string) {
 	}
 }
 
+// Logout
+//
+//	@Title			Logout
+//	@Description	Logout
+//	@Success		200			"success"
+//	@router			/ [put]
 func (ctl *LoginController) Logout() {
 	defer logs.Info("[LoginController][Logout] Username:", ctl.Username)
 
